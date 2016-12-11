8928d3e14edea038eb30e962e0fef5b7650edfa7
Prasong Kanokratmanee <prasong.k@garena.co.th>
2016-12-11 23:37:43 +0700

    first

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var app = express()
+var jade = require('jade')
+var fs = require('fs')
+
+app.set('view engine', 'pug')
+app.use('/static', express.static('public'))
+
+app.get('/:file', function (req, res) {
+  const file = req.params.file;
+
+  if (fs.existsSync('views/'+file+'.pug'))
+    res.render(file)
+  else
+    res.render('default')
+
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
