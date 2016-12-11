486e3184ed7ebd88d15d8ed4bdde29c88e5d2ec7
Prasong Kanokratmanee <prasong.k@garena.co.th>
2016-12-12 00:20:37 +0700

    index access to show all view files

--- server.js
+++ server.js
@@ -6,11 +6,24 @@ var fs = require('fs')
 app.set('view engine', 'pug')
 app.use('/static', express.static('public'))
 
-app.get('/:file', function (req, res) {
+app.get('/:file?', function (req, res) {
   const file = req.params.file;
+  const viewsDir = 'views';
 
-  if (fs.existsSync('views/'+file+'.pug'))
+  if (fs.existsSync(viewsDir+'/'+file+'.pug'))
     res.render(file)
+  else if (file === undefined)
+    fs.readdir(viewsDir, (err, files) => {
+      //remove default view in list
+      let index = files.indexOf('default.pug');
+      files.splice(index, 1);
+
+      files = files.map( (t) => t.replace(".pug", "") )
+      files = files.map( (t) => '<a href="/' + t + '">' + t + '</a>' )
+
+      let list = files.join("<br/>")
+      res.send(list)
+    })
   else
     res.render('default')
 
